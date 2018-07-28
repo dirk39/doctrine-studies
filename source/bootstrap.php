9dcfb5325172be726e079cec59e39da18be51a18
Dirk39 <andreaquin1990@gmail.com>
2018-07-28 11:15:30 +0200

    created bootstrap.php

---
+++ source/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration([__DIR__."/src"], $isDevMode);
+
+$conn = [
+  "driver" => "pdo_sqlite",
+  "path" => __DIR__.'/db.sqlite'
+];
+
+$entityManager = EntityManager::create($conn, $config);

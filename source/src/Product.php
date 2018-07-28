02a2fe1e3d9d467056758a151b66e428308d569a
Dirk39 <andreaquin1990@gmail.com>
2018-07-28 12:39:44 +0200

    wipping

---
+++ source/src/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @Entity @Table(name="products")
+ */
+class Product
+{
+  /**
+   * @Id @Column(type="integer") @GeneratedValue
+   */
+  protected $id;
+
+  /**
+   * @Column(type="string")
+   */
+  protected $name;
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name)
+  {
+    $this->name = $name;
+  }
+}
